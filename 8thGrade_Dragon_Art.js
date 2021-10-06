804a6196537fde717e8d156df5fb68034d436f60
Dovcollegeport <92009405+Dovcollegeport@users.noreply.github.com>
2021-10-06 12:30:25 -0400

    Create 8thGrade_Dragon_Art.js

---
+++ 8thGrade_Dragon_Art.js
@@ -0,0 +1,29 @@
+//dragons are cool
+
+background(0, 247, 255);
+noStroke();
+//body
+fill(255, 0, 0);
+rect(338,300,10,64);
+rect(85,300,10,64);
+rect(295,356,43,6);
+rect(44,356,43,6);
+fill(168, 156, 168);
+ellipse(218,295,322,107);
+rect(79,72,67,200);
+ellipse(159,76,206,76);
+//facial features
+fill(255, 255, 255);
+ellipse(196,72,40,40);
+fill(2, 0, 0);
+ellipse(200,72,10,10);
+fill(255, 0, 0);
+ellipse(247,80,34,45);
+stroke(255, 0, 0);
+line(246,90,310,179);
+ellipse(308,179,20,02);
+//scale
+stroke(2, 0, 0);
+fill(204, 0, 255);
+triangle(79,140,101,180,124,178);
+
